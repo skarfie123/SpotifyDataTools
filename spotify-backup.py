38953b5b4f7b70c8e19ecfbed509f39e8f078aec
skarfie123 <rahulpai@rocketmail.com>
2021-06-14 19:45:25 +0100

    check duplicates in same script

--- spotify-backup.py
+++ spotify-backup.py
@@ -5,6 +5,7 @@ import json
 import logging
 import os
 from io import TextIOWrapper
+from ssl import wrap_socket
 
 import click
 import coloredlogs
@@ -32,32 +33,55 @@ def load_playlist(spotify: SpotifyAPI, me, playlist):
 
 
 def write_playlist(f: TextIOWrapper, playlist):
-    """Write playlist as TabSeperatedValues to a file"""
+    """Write playlist to a file"""
     f.write(playlist["name"] + "\n")
     for i, track in enumerate(playlist["tracks"]):
         if track["track"] is None:
             logging.error(f'{track}["track"] is None')
             continue
-        f.write(
-            "{index}\t{name}\t{artists}\t{album}\t{uri}\n".format(
-                index=i + 1,
-                uri=track["track"]["uri"],
-                name=track["track"]["name"],
-                artists=", ".join(
-                    [artist["name"] for artist in track["track"]["artists"]]
-                ),
-                album=track["track"]["album"]["name"],
-            )
+        write_track(f, track, i + 1)
+
+
+def write_duplicates(f: TextIOWrapper, playlist):
+    """Write duplicates to a file"""
+    f.write(playlist["name"] + " duplicates\n")
+    for i, track1 in enumerate(playlist["tracks"]):
+        if track1["track"] is None:
+            continue
+        for j, track2 in enumerate(playlist["tracks"]):
+            if track2["track"] is None:
+                continue
+            if (
+                i < j
+                and track1["track"]["name"][:5] == track2["track"]["name"][:5]
+                and track1["track"]["artists"] == track2["track"]["artists"]
+            ):
+                write_track(f, track1, i + 1)
+                write_track(f, track2, j + 1)
+
+
+def write_track(f: TextIOWrapper, track, index):
+    """Write a track as TabSeperatedValues to a file"""
+    f.write(
+        "{index}\t{name}\t{artists}\t{album}\t{uri}\n".format(
+            index=index,
+            uri=track["track"]["uri"],
+            name=track["track"]["name"],
+            artists=", ".join([artist["name"] for artist in track["track"]["artists"]]),
+            album=track["track"]["album"]["name"],
         )
+    )
 
 
-def playlist_filename(playlist, format: str):
-    return "".join([x if x.isalnum() else "_" for x in playlist["name"]]) + "." + format
+def playlist_filename(playlist):
+    return "".join([x if x.isalnum() else "_" for x in playlist["name"]])
 
 
-def confirm_overwrite(filename):
-    return not os.path.exists(filename) or click.confirm(
-        f"{filename} already exists, do you want to overwrite?"
+def confirm_overwrite(filename: str, yes: bool = False):
+    return (
+        yes
+        or not os.path.exists(filename)
+        or click.confirm(f"{filename} already exists, do you want to overwrite?")
     )
 
 
@@ -83,7 +107,7 @@ def main():
     parser.add_argument(
         "--folder",
         default="backup",
-        help="folder to save each file (default: backup)",
+        help="folder to save each file, normal mode only (default: backup)",
     )
     parser.add_argument(
         "--format",
@@ -103,7 +127,21 @@ def main():
         action="store_true",
         help="save only playlists owned by you (default: False)",
     )
-    parser.set_defaults(single=False, mine=False)
+    parser.add_argument(
+        "--cd",
+        "--check-duplicates",
+        dest="checkDuplicates",
+        action="store_true",
+        help="check for duplicates, normal mode only (default: False)",
+    )
+    parser.add_argument(
+        "-y",
+        "--yes",
+        dest="yes",
+        action="store_true",
+        help="say yes to all overwrite confirmations (default: False)",
+    )
+    parser.set_defaults(single=False, mine=False, checkDuplicates=False, yes=False)
     parser.add_argument("file", help="output filename for single file mode", nargs="?")
     args = parser.parse_args()
 
@@ -159,7 +197,7 @@ def main():
             playlists = [playlists[choice]]
 
     if args.single:
-        if args.file and not confirm_overwrite(args.file):
+        if args.file and not confirm_overwrite(args.file, args.yes):
             args.file = None
 
         # If they didn't give a filename, then just prompt them.
@@ -167,7 +205,7 @@ def main():
             args.file = input("Enter a file name (e.g. playlists.txt): ")
             args.format = args.file.split(".")[-1]
 
-            if args.file and not confirm_overwrite(args.file):
+            if args.file and not confirm_overwrite(args.file, args.yes):
                 args.file = None
 
         for playlist in playlists:
@@ -189,10 +227,10 @@ def main():
 
         for playlist in playlists:
             filename = os.path.join(
-                args.folder, playlist_filename(playlist, args.format)
+                args.folder, playlist_filename(playlist) + "." + args.format
             )
 
-            if not confirm_overwrite(filename):
+            if not confirm_overwrite(filename, args.yes):
                 continue
 
             load_playlist(spotify, me, playlist)
@@ -204,6 +242,19 @@ def main():
                     json.dump(playlist, f)
                 elif args.format == "txt":
                     write_playlist(f, playlist)
+
+            if args.checkDuplicates:
+                duplicates_filename = os.path.join(
+                    args.folder, playlist_filename(playlist) + "_duplicates.txt"
+                )
+
+                if not confirm_overwrite(duplicates_filename, args.yes):
+                    continue
+
+                with open(duplicates_filename, "w", encoding="utf-8") as f:
+                    logging.info("Writing file: " + f.name)
+
+                    write_duplicates(f, playlist)
 
 
 if __name__ == "__main__":
